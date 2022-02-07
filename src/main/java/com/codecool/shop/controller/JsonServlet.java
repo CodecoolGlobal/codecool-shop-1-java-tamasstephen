f308bc72f4cd68d357ed9b46bced9b5319f3ca35
tamasstephen <kicsitnagyon@gmail.com>
2022-02-07 20:07:53 +0100

    chore: Implement order update on the server side

--- src/main/java/com/codecool/shop/controller/JsonServlet.java
+++ src/main/java/com/codecool/shop/controller/JsonServlet.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.ProductRef;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,8 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.List;
 
-@WebServlet(name = "JsonServlet", urlPatterns = {"/api/products", "/api/product", "/json"}, loadOnStartup = 2)
+@WebServlet(name = "JsonServlet", urlPatterns = {"/api/update", "/api/add", "/json"}, loadOnStartup = 2)
 public class JsonServlet extends HttpServlet {
 
     @Override
@@ -38,10 +44,25 @@ public class JsonServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         BufferedReader reader = request.getReader();
+        String url = request.getRequestURL().toString();
+        Order cart = Order.getInstance();
+        Type myType = new TypeToken<ProductRef>(){}.getType();
+        String value = "";
         String line;
         while ((line = reader.readLine()) != null){
-            System.out.println(line);
+           value += line;
         }
+
+        ProductRef skeleton = new Gson().fromJson(value, myType);
+        System.out.println(skeleton.getAmount());
+        if (url.contains("add")){
+            cart.addProduct(skeleton.getId());
+        } else if (url.contains("update")){
+            cart.updateProduct(skeleton.getId(), skeleton.getAmount());
+        }
+        System.out.println(cart.toString());
+
+
 
         doGet(request, response);
     }
