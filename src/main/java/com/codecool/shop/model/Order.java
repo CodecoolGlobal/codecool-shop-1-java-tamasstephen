bfcb4531915da4d967e613167d33a4cb683ffd10
tamasstephen <kicsitnagyon@gmail.com>
2022-02-07 20:06:47 +0100

    refactor: Replace string id-s with Integer in Order class

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,6 +1,6 @@
 package com.codecool.shop.model;
 
-import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.ProductDao;
 import com.google.gson.Gson;
 
 import java.util.HashMap;
@@ -8,16 +8,20 @@ import java.util.Map;
 
 public class Order {
 
-    private final Map<String, Integer> cart;
-    private final ProductDaoMem products;
+    private final Map<Integer, Integer> cart;
+    private final ProductDao products;
     private static Order order;
 
-    private Order(ProductDaoMem products) {
+    private Order(ProductDao products) {
         this.cart = new HashMap<>();
         this.products = products;
     }
 
-    public Order getInstance(ProductDaoMem products){
+    public static Order getInstance(){
+        return order;
+    }
+
+    public static Order getInstance(ProductDao products){
 
         if (order == null){
 
@@ -29,11 +33,11 @@ public class Order {
 
     }
 
-    private boolean hasProduct(String id){
+    private boolean hasProduct(Integer id){
         return cart.get(id) != null;
     }
 
-    public void addProduct(String id){
+    public void addProduct(Integer id){
 
         if (hasProduct(id)){
 
@@ -48,7 +52,7 @@ public class Order {
 
     }
 
-    public void updateProduct(String id, int amount){
+    public void updateProduct(Integer id, int amount){
         cart.put(id, amount);
     }
 
@@ -64,6 +68,18 @@ public class Order {
         // TODO: create a productCartModel -> just the final values (unit price, sub-total, name, description, id)
 
         return null;
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder content = new StringBuilder();
+        for (Integer value: cart.keySet()){
+            content.append(String.format("{%d : %d}", value, cart.get(value)));
+        }
+
+        return content.toString();
+
     }
 
 
