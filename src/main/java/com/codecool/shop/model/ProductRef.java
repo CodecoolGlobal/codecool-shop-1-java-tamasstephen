5eb8ccef8a2c97c623451ea1004c61baa63d17e6
tamasstephen <kicsitnagyon@gmail.com>
2022-02-07 20:06:17 +0100

    chore: Add model class to retrieve product data

---
+++ src/main/java/com/codecool/shop/model/ProductRef.java
@@ -0,0 +1,14 @@
+package com.codecool.shop.model;
+
+public class ProductRef {
+    int id;
+    int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
