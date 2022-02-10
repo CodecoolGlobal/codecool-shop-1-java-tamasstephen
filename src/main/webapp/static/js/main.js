366aaed3263bf4a7290632af6834ec817eee8b42
matekaszas13 <matekaszas17@gmail.com>
2022-02-10 10:20:46 +0100

    Refactor: Call addEventOnCartButton after change the product content

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,11 +1,9 @@
 import {addProductToCart, addEventOnSuppliers, addEventOnCategory, addEventOnLogo} from "./controller/listen.js";
-import { openModal } from "./controller/listen.js";
+import { openModal, setUpProductButtons} from "./controller/listen.js";
 import {updateCartTooltip} from "./view/view.js";
 import {dataHandler} from "./data/dataHandler.js";
 
 init();
-
-//:TODO Should call event on add cart button after change the content
 
 async function init(){
     setUpProductButtons();
@@ -17,10 +15,7 @@ async function init(){
     localStorage.clear();
 }
 
-function setUpProductButtons(fnc){
-    const buttons = document.querySelectorAll(".add-to-cart");
-    buttons.forEach(button => button.addEventListener("click", addProductToCart));
-}
+
 
 function setUpCartButton(){
     const cartButton = document.querySelector("#cart");
